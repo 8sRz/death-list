bcbcb072fcd52683f215faf99fa071f40dd50e12
8sRz <dk9t6_im1vlp5y0rl8zy@redact.me>
2025-01-09 10:33:12 -0600

    Update generate_lists.py

--- generate_lists.py
+++ generate_lists.py
@@ -9,4 +9,4 @@ with open(os.path.join(folder_path, 'ABP.txt'), 'w') as file:
     file.writelines([f"||{line.strip()}^\n" for line in lines])
 
 with open(os.path.join(folder_path, 'personalDNSfilter.txt'), 'w') as file:
-    file.writelines([f"*{line}" for line in lines])
+    file.writelines([f"*.{line}" for line in lines])
