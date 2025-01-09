5acde76abaebecf419063306674b11c55f013ba5
8sRz <dk9t6_im1vlp5y0rl8zy@redact.me>
2025-01-08 22:56:25 -0600

    Create generate_lists.py

---
+++ generate_lists.py
@@ -0,0 +1,12 @@
+import os
+
+folder_path = "./formats"
+
+with open(os.path.join(folder_path, 'base.txt'), 'r') as file:
+    lines = file.readlines()
+
+with open(os.path.join(folder_path, 'ABP.txt'), 'w') as file:
+    file.writelines([f"||{line.strip()}^\n" for line in lines])
+
+with open(os.path.join(folder_path, 'personalDNSfilter.txt'), 'w') as file:
+    file.writelines([f"*{line}" for line in lines])
